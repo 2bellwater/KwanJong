628c0df7251c42532dd38dbe5952840407c3d79c
ImYeol <imyeol115@gmail.com>
2019-02-20 00:24:05 +0900

    changed https to http in Vuejs

--- src/plugins/socketPlugin.js
+++ src/plugins/socketPlugin.js
@@ -1,7 +1,7 @@
 import Vue from 'vue';
 import io from 'socket.io-client';
 
-const socket = io('https://jongsoochat.herokuapp.com:3000');
+const socket = io('http://jongsoochat.herokuapp.com:3000');
 
 
 const SocketPlugin = {
